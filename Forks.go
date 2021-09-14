39b0b732feebb7159ae0812cf4563243061cbc27
steven <stwa@itu.dk>
2021-09-14 11:41:22 +0200

    Implemented Forkiphize

--- Forks.go
+++ Forks.go
@@ -1,1 +1,35 @@
-Forks
+package main
+import (
+	"strconv"
+)
+type Fork struct{
+	used bool
+	in chan int
+}
+
+func forkiphize(inp chan int){
+	uses := 0
+	usedBy :=-1
+
+	for true{
+		select{
+		case a:= <- inp:
+			if(a == 42){
+				uses++
+			}else if(a == 0){
+				output <- strconv.Itoa(uses)
+			}else if(a >=10 && a<15){
+				usedBy = a-10
+			}else if(a == -3){
+				usedBy = -1;
+			}else if(a == 1){
+				if(usedBy == -1){
+					output <-"Not in use"
+				}else{
+					output <-strconv.Itoa(usedBy)
+				}
+			}
+		default:
+		}
+}
+}
--- philosopher.go
+++ philosopher.go
@@ -9,39 +9,53 @@ import (
 )
 
 var philos [5]chan int 
-var forks [5]bool
+//var forks [5]bool
+var forks [5]Fork
 var mu sync.Mutex
 var output chan string
 
 func main() {
 	output = make(chan string)
 	for i := 0; i < 5; i++ {
+		forks[i] = Fork{false, make(chan int)}
 		philos[i] = make(chan int)
+		go forkiphize(forks[i].in)
 		go Philosophize(i, philos[i])
 	}
 	for true {
-		var qu0, qu1 int
-		fmt.Scanln(&qu0, &qu1)
+		var qu0, qu1, qu2 int
+		fmt.Scanln(&qu0, &qu1, &qu2)
 		if (qu0 == -1) {
 			break
 		}
+		if(qu2 == 0){
+		  philos[qu0] <- qu1
+		}else{
+			forks[qu0].in <- qu1
+		}
 
-		philos[qu0] <- qu1
 
 		fmt.Println(<- output)
 	}
 }
 
-func TakeFork(f int) {
-	forks[f] = true
+func TakeFork(f,p int) {
+	forks[f].used = true
+	forks[f].in <-p
 }
 
 func PutFork(f int) {
-	forks[f] = false
+	forks[f].used = false
+	forks[f].in <- -3
 }
 
 func CanTake(f int) bool {
-	return !forks[f]
+	return !forks[f].used
+}
+
+func useforks(r int, n int){
+	forks[n].in <- 42
+	forks[r].in <- 42
 }
 
 func Philosophize(n int, input chan int) {
@@ -51,11 +65,12 @@ func Philosophize(n int, input chan int) {
 	for true {
 		mu.Lock()
 		if CanTake(n) {
-			TakeFork(n)
+			TakeFork(n, n+10)
 			if CanTake(r) {
-				TakeFork(r)
+				TakeFork(r,n+10)
 				eating = true
 				eaten++
+				useforks(r,n)
 			} else {
 				PutFork(n)
 			}
